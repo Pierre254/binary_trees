2d6ca194e2e874b176706acf9b3c400269ee4018
Pierre254 <ptruter@gmail.com>
2024-03-28 07:58:26 -0700

    search

---
+++ 113-bst_search.c
@@ -0,0 +1,24 @@
+97% of storage used … If you run out, you can't create, edit and upload files. Get 100 GB of storage for R 34.99 R 8.99/month for 3 months.
+113-bst_search.c
+#include "binary_trees.h"
+
+/**
+ * bst_search - Searches for a value in a binary search tree.
+ * @tree: A pointer to the root node of the BST to search.
+ * @value: The value to search for in the BST.
+ *
+ * Return: If the tree is NULL or the value is not found, NULL.
+ *         Otherwise, a pointer to the node containing the value.
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree != NULL)
+	{
+		if (tree->n == value)
+			return ((bst_t *)tree);
+		if (tree->n > value)
+			return (bst_search(tree->left, value));
+		return (bst_search(tree->right, value));
+	}
+	return (NULL);
+}
